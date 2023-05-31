c37c8448be329816774655ebf2a266fb410a74fd
GYME <gymeclub@gmail.com>
2023-05-31 07:15:47 +0300

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/undergroundBlock/',
   plugins: [react(), tsconfigPaths()],
 })
 
